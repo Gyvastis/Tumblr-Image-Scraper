48fb117b7fec22bf6a2ced550d733908135a38b0
Vaidas <shibalba1@gmail.com>
2015-03-01 22:56:39 +0200

    Fix rege

--- tumblr_scraper.rb
+++ tumblr_scraper.rb
@@ -2,10 +2,11 @@
 require 'open-uri'
 require 'digest/md5'
 
-regex = /(http|https):\/\/\d+.media.tumblr.com\/(.*).(jpg|png|gif)/
-num = 1
+regex = /(http|https):\/\/\d+.media.tumblr.com\/[^\/]+\/tumblr_(.*).(jpg|png|gif)">/
+page = 1
 count = 1
 max = 9000
+# sleep_t = 3
 done = false
 
 # make cli arg
@@ -21,6 +22,11 @@ elsif ARGV.size == 3 then
 	user = ARGV[0]
 	basedir = ARGV[1]
 	max = ARGV[2].to_i
+elsif ARGV.size == 3 then
+	user = ARGV[0]
+	basedir = ARGV[1]
+	max = ARGV[2].to_i
+
 else
 	puts "usage: ruby tumblr_scraper.rb 'username' '/path/to/save/' 'just take X photos'"
 	exit
@@ -28,8 +34,8 @@ end
 
 while not done do
 	arr = Array.new
-	puts "[*] scraping page #{num}"
-	page = open('http://'+user+'.tumblr.com/page/'+num.to_s) do |p|
+	puts "[*] scraping page #{page}"
+	page = open('http://'+user+'.tumblr.com/page/'+page.to_s) do |p|
 		p.each_line do |line|
 			arr.push($&) if line =~ regex
 		end
@@ -38,13 +44,14 @@ while not done do
 	done = true if arr.size == 0
 
 	arr.each do |pic|
+		pic = pic.chomp('">')
 		f = File.new("#{basedir}#{Digest::MD5.hexdigest(pic)}.#{pic.scan(/(jpg|png|gif)/)[0][0]}", "wb") 
 		f.write(open(pic).read)
 		f.close
 		puts "[+] wrote pic #{count}"
 		count += 1
 		exit if count == max
-		sleep 1 + rand(2)
+		# sleep rand(sleep_t)
 	end
-	num += 1
+	page += 1
 end
